2b15a0fce1a0396ee915901d07e0a349b08e6127
github username <github email address>
2019-01-28 15:57:13 +0000

    learn submit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector (selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget () {
+  let nested = document.getElementById('nested')
+  return nested.querySelector('.target')
+}
+
+function increaseRankBy (n) {
+  let list = document
+    .getElementById('app')
+    .querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = (parseInt(list[i].innerHTML) + n).toString()
+  }
+}
+
+
+function deepestChild() {
+    return document.getElementById('grand-node').querySelector('div div div div div')
+}
